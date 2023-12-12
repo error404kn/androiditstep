28b52db3e53398d6208f7db232ee8fa431ee66dd
error404kn <scandaluzumaki@gmail.com>
2023-12-12 15:17:14 +0400

    IT Step

---
+++ app/src/main/java/com/example/myapplication/ProfileFragment.kt
@@ -0,0 +1,48 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import com.example.myapplication.databinding.FragmentProfileBinding
+
+class ProfileFragment : Fragment() {
+
+
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
+    private val viewModel: ProfileViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
+    }
+
+    private fun init(){
+        binding.name.text = viewModel.name
+        binding.editProfileLL.setOnClickListener{
+            findNavController().navigate(R.id.action_profileFragment_to_editProfileFragment8)
+        }
+        binding.logout.setOnClickListener{
+            binding.name.text = null
+        }
+    }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
+    }
+
+}
